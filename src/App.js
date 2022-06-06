1fbf0b944d73a239f52ca3464cf26be0a117edbd
Asad Mahmood <asadmhmd1234@gmail.com>
2022-06-06 18:12:26 +0530

    Add general information form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import Edit from "./components/Edit";
+import Preview from "./components/Preview";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      general: {
+        name: "",
+        email: "",
+        phone: "",
+      },
+      isPreviewEnabled: false,
+    };
+    this.toggleViewMode = this.toggleViewMode.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(changedData) {
+    console.log(changedData);
+    this.setState({ ...changedData });
+  }
+  toggleViewMode() {
+    this.setState((prevState) => {
+      return {
+        isPreviewEnabled: !prevState.isPreviewEnabled,
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <input type="checkbox" name="mode" onClick={this.toggleViewMode} />
+        {this.state.isPreviewEnabled ? (
+          <Preview />
+        ) : (
+          <Edit formData={this.state} changeHandler={this.handleChange} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
