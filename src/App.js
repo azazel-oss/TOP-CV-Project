585ceeefeb68b2150d312b51f2c3f9a7db265a3e
Asad Mahmood <asadmhmd1234@gmail.com>
2022-06-11 03:12:20 +0530

    Change App.js from class based components to function based components

--- src/App.js
+++ src/App.js
@@ -1,109 +1,84 @@
-import { Component } from "react";
+import { useState } from "react";
 import Edit from "./components/Edit";
 import Preview from "./components/Preview";
 import "./styles/App.css";
 import "./styles/checkbox.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      general: {
-        name: "",
-        email: "",
-        phone: "",
-      },
-      education: [],
-      experience: [],
-      isPreviewEnabled: false,
-    };
-    this.toggleViewMode = this.toggleViewMode.bind(this);
-    this.handleGeneralChange = this.handleGeneralChange.bind(this);
-    this.handleEducationAdd = this.handleEducationAdd.bind(this);
-    this.handleExperienceAdd = this.handleExperienceAdd.bind(this);
-    this.handleExperienceRemove = this.handleExperienceRemove.bind(this);
-    this.handleEducationRemove = this.handleEducationRemove.bind(this);
+function App() {
+  const [general, setGeneral] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+  const [education, setEducation] = useState([]);
+  const [experience, setExperiece] = useState([]);
+  const [isPreviewEnabled, setIsPreviewEnable] = useState(false);
+  function handleGeneralChange(changedData) {
+    setGeneral(changedData.general);
   }
-  handleGeneralChange(changedData) {
-    this.setState({ ...changedData });
-  }
-  handleEducationAdd(newEducation) {
-    this.setState((prevState) => {
-      return {
-        education: [...prevState.education, newEducation],
-      };
+  function handleEducationAdd(newEducation) {
+    setEducation((prevState) => {
+      return [...prevState, newEducation];
     });
   }
-  handleExperienceAdd(newExperience) {
-    this.setState((prevState) => {
-      return {
-        experience: [...prevState.experience, newExperience],
-      };
+  function handleExperienceAdd(newExperience) {
+    setExperiece((prevState) => {
+      return [...prevState, newExperience];
     });
   }
-  handleEducationRemove(id) {
-    this.setState((prevState) => {
-      return {
-        education: prevState.education.filter((element) => element.id !== id),
-      };
+  function handleEducationRemove(id) {
+    setEducation((prevState) => {
+      return prevState.filter((element) => element.id !== id);
     });
   }
-  handleExperienceRemove(id) {
-    this.setState((prevState) => {
-      return {
-        experience: prevState.experience.filter((element) => element.id !== id),
-      };
+  function handleExperienceRemove(id) {
+    setExperiece((prevState) => {
+      return prevState.filter((element) => element.id !== id);
     });
   }
-  toggleViewMode() {
-    this.setState((prevState) => {
-      return {
-        isPreviewEnabled: !prevState.isPreviewEnabled,
-      };
-    });
+  function toggleViewMode() {
+    setIsPreviewEnable((prevState) => !prevState);
   }
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <h1>CV Maker</h1>
-          <p>Fill up the details and make a cv just by a click</p>
-        </header>
-        <main>
-          <div className="flex">
-            <span>Editing Mode</span>
-            <label className="switch" htmlFor="checkbox">
-              <input
-                type="checkbox"
-                id="checkbox"
-                name="mode"
-                onClick={this.toggleViewMode}
-              />
-              <div className="slider round"></div>
-            </label>
-            <span>Preview Mode</span>
-          </div>
-          {this.state.isPreviewEnabled ? (
-            <Preview previewData={this.state} />
-          ) : (
-            <Edit
-              formData={this.state}
-              generalChangeHandler={this.handleGeneralChange}
-              educationChangeHandler={this.handleEducationAdd}
-              experienceChangeHandler={this.handleExperienceAdd}
-              removeEducationHandler={this.handleEducationRemove}
-              removeExperienceHandler={this.handleExperienceRemove}
+  return (
+    <div className="container">
+      <header>
+        <h1>CV Maker</h1>
+        <p>Fill up the details and make a cv just by a click</p>
+      </header>
+      <main>
+        <div className="flex">
+          <span>Editing Mode</span>
+          <label className="switch" htmlFor="checkbox">
+            <input
+              type="checkbox"
+              id="checkbox"
+              name="mode"
+              onClick={toggleViewMode}
             />
-          )}
-        </main>
-        <footer>
-          <a href="https://www.github.com/azazel-oss">
-            Copyright &copy; Asad Mahmood
-          </a>
-        </footer>
-      </div>
-    );
-  }
+            <div className="slider round"></div>
+          </label>
+          <span>Preview Mode</span>
+        </div>
+        {isPreviewEnabled ? (
+          <Preview previewData={{ general, education, experience }} />
+        ) : (
+          <Edit
+            formData={{ general, education, experience }}
+            generalChangeHandler={handleGeneralChange}
+            educationChangeHandler={handleEducationAdd}
+            experienceChangeHandler={handleExperienceAdd}
+            removeEducationHandler={handleEducationRemove}
+            removeExperienceHandler={handleExperienceRemove}
+          />
+        )}
+      </main>
+      <footer>
+        <a href="https://www.github.com/azazel-oss">
+          Copyright &copy; Asad Mahmood
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
