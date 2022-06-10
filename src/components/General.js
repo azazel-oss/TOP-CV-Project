58d4af377a03ae7299a4b3ea74f510d67363d886
Asad Mahmood <asadmhmd1234@gmail.com>
2022-06-11 03:54:13 +0530

    Update General.js and Preview.js to functional components

--- src/components/General.js
+++ src/components/General.js
@@ -1,52 +1,44 @@
-import { Component } from "react";
-
-class General extends Component {
-  constructor() {
-    super();
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(event) {
+function General(props) {
+  function handleChange(event) {
     const data = {
       general: {
-        ...this.props.data,
+        ...props.data,
       },
     };
     data.general[event.target.name] = event.target.value;
-    this.props.changeHandler(data);
+    props.changeHandler(data);
   }
-  render() {
-    return (
-      <div>
-        <h3>General Information</h3>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={this.props.data.name}
-          onChange={this.handleChange}
-        />
-        <br />
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={this.props.data.email}
-          onChange={this.handleChange}
-        />
-        <br />
-        <label htmlFor="phone">Phone:</label>
-        <input
-          type="text"
-          id="phone"
-          name="phone"
-          value={this.props.data.phone}
-          onChange={this.handleChange}
-        />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h3>General Information</h3>
+      <label htmlFor="name">Name:</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={props.data.name}
+        onChange={handleChange}
+      />
+      <br />
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        value={props.data.email}
+        onChange={handleChange}
+      />
+      <br />
+      <label htmlFor="phone">Phone:</label>
+      <input
+        type="text"
+        id="phone"
+        name="phone"
+        value={props.data.phone}
+        onChange={handleChange}
+      />
+    </div>
+  );
 }
 
 export default General;
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,53 +1,49 @@
-import { Component } from "react";
-
-class Preview extends Component {
-  render() {
-    return (
-      <>
-        <h2>Resume</h2>
-        <section>
-          <h3>Personal Information</h3>
-          <div>{this.props.previewData.general.name}</div>
-          <div>{this.props.previewData.general.email}</div>
-          <div>{this.props.previewData.general.phone}</div>
-        </section>
-        <section>
-          <h3>Educational Information</h3>
-          <div>
-            {this.props.previewData.education.map((element, index) => {
-              return (
-                <div key={index}>
-                  <div>{element.schoolName}</div>
-                  <div>{element.studyTitle}</div>
-                  <div>{element.studyDate}</div>
-                </div>
-              );
-            })}
-          </div>
-        </section>
-        <section>
-          <h3>Job Experience Information</h3>
-          <div>
-            {this.props.previewData.experience.map((element) => {
-              return (
+function Preview(props) {
+  return (
+    <>
+      <h2>Resume</h2>
+      <section>
+        <h3>Personal Information</h3>
+        <div>{props.previewData.general.name}</div>
+        <div>{props.previewData.general.email}</div>
+        <div>{props.previewData.general.phone}</div>
+      </section>
+      <section>
+        <h3>Educational Information</h3>
+        <div>
+          {props.previewData.education.map((element, index) => {
+            return (
+              <div key={index}>
+                <div>{element.schoolName}</div>
+                <div>{element.studyTitle}</div>
+                <div>{element.studyDate}</div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+      <section>
+        <h3>Job Experience Information</h3>
+        <div>
+          {props.previewData.experience.map((element) => {
+            return (
+              <div>
+                <div>{element.companyName}</div>
+                <div>{element.positionTitle}</div>
+                <div>{element.startDate}</div>
+                <div>{element.endDate}</div>
                 <div>
-                  <div>{element.companyName}</div>
-                  <div>{element.positionTitle}</div>
-                  <div>{element.startDate}</div>
-                  <div>{element.endDate}</div>
-                  <div>
-                    {element.jobTasks.split(",").map((task) => (
-                      <li>{task.trim()}</li>
-                    ))}
-                  </div>
+                  {element.jobTasks.split(",").map((task) => (
+                    <li>{task.trim()}</li>
+                  ))}
                 </div>
-              );
-            })}
-          </div>
-        </section>
-      </>
-    );
-  }
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Preview;
